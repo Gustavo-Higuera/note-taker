c16ab0c56a0fab16b9a86f528026f57b83a1bc39
Coding Padawan <gustavohiguera9715@gmail.com>
2022-09-04 16:33:17 -0700

    added routes to html files

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const PORT = 3001;
 const app = express();
-const notes = require("./public/notes.html");
+const htmlRoutes = require('./routes/html-routes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Your API server is listening on port ${PORT}`);
+  console.log(`App is listening at http://localhost:${PORT}`);
 })
